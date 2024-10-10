1b285779bea2d6ab7b4d73bf72aa4c3a8c3e4445
marcilustra <theknifebox69@gmail.com>
2024-10-10 12:55:24 +0300

    PDF File for submission

--- ASimpleCalculator/My Project/Application.Designer.vb
+++ ASimpleCalculator/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ASimpleCalculator.frmOptionButtons
+            Me.MainForm = Global.ASimpleCalculator.frmCheckBoxes
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
